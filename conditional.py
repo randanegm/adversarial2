f7e1e39fdede42feaab0dec0c9f5e5e6024de63b
Jon Gauthier <jon@gauthiers.net>
2015-02-16 12:09:54 -0800

    Add "conditional" module with Generator implementation, skeletons for rest

---
+++ conditional.py
@@ -0,0 +1,109 @@
+
+from pylearn2.models.mlp import MLP
+from pylearn2.space import CompositeSpace, VectorSpace
+from theano import tensor as T
+
+from adversarial import AdversaryPair, Generator
+
+
+class ConditionalAdversaryPair(AdversaryPair):
+    def __init__(self, *args, **kwargs):
+        super(ConditionalAdversaryPair, self).__init__(*args, **kwargs)
+
+
+class ConditionalGenerator(Generator):
+    def __init__(self, condition_space, noise_dim=100, *args, **kwargs):
+        super(Generator, self).__init__(*args, **kwargs)
+
+        self.noise_dim = noise_dim
+        self.noise_space = VectorSpace(dim=self.noise_dim)
+
+        self.condition_space = condition_space
+
+        self.input_space = CompositeSpace(self.noise_space, self.condition_space)
+        self.mlp.set_input_space(self.input_space)
+
+    def get_input_space(self):
+        return self.input_space
+
+    def sample_and_noise(self, conditional_data, default_input_include_prob=1., default_input_scale=1.,
+                         all_g_layers=False):
+        """
+        Retrieve a sample (and the noise used to generate the sample)
+        conditioned on some input data.
+
+        Parameters
+        ----------
+        conditional_data: member of self.condition_space
+            A minibatch of conditional data to feedforward.
+
+        default_input_include_prob: float
+            WRITEME
+
+        default_input_scale: float
+            WRITEME
+
+        all_g_layers: boolean
+            If true, return all generator layers in `other_layers` slot
+            of this method's return value. (Otherwise returns `None` in
+            this slot.)
+
+        Returns
+        -------
+        net_output: 3-tuple
+            Tuple of the form `(sample, noise, other_layers)`.
+        """
+
+        num_samples = len(conditional_data)
+
+        noise = self.get_noise((num_samples, self.noise_dim))
+        # TODO necessary?
+        formatted_noise = self.noise_space.format_as(noise, self.noise_space)
+
+        # Build inputs: concatenate noise with conditional data
+        inputs = T.concatenate((noise, conditional_data), axis=1)
+
+        # Feedforward
+        if all_g_layers:
+            rval = self.mlp.dropout_fprop(inputs, default_input_include_prob=default_input_include_prob,
+                                          default_input_scale=default_input_scale, return_all=all_g_layers)
+            other_layers, rval = rval[:-1], rval[-1]
+        else:
+            rval = self.mlp.dropout_fprop(inputs, default_input_include_prob=default_input_include_prob,
+                                          default_input_scale=default_input_scale)
+            other_layers = None
+
+        return rval, formatted_noise, other_layers
+
+    def sample(self, conditional_data, **kwargs):
+        sample, _, _ = self.sample_and_noise(conditional_data, **kwargs)
+        return sample
+
+    def get_monitoring_channels(self, data):
+        # TODO
+        return {}
+
+
+class ConditionalDiscriminator(MLP):
+    def __init__(self, input_data_space, input_condition_space, *args, **kwargs):
+        """
+        A discriminator acting within a cGAN which may "condition" on
+        extra information.
+
+        Parameters
+        ----------
+        input_data_space : pylearn2.space.CompositeSpace
+            Space which contains the empirical / model-generated data
+
+        input_condition_space : pylearn2.space.CompositeSpace
+            Space which contains the extra data being conditioned on
+
+        kwargs : dict
+            Passed on to MLP superclass.
+        """
+
+        assert 'input_space' not in kwargs
+
+        super(ConditionalDiscriminator, self).__init__(
+            input_space=CompositeSpace(input_data_space, input_condition_space),
+            *args, **kwargs)

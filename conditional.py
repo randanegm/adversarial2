73a2dd2ef71a018bd029200293d0b1e90874f844
Jon Gauthier <jon@gauthiers.net>
2015-02-18 20:14:27 -0800

    Implement ConditionalAdversaryCost

--- conditional.py
+++ conditional.py
@@ -160,5 +160,75 @@ class ConditionalAdversaryCost(AdversaryCost2):
         super(ConditionalAdversaryCost, self).__init__(**kwargs)
 
     def get_samples_and_objectives(self, model, data):
-        # TODO
-        print model, data
+        space, sources = self.get_data_specs(model)
+        space.validate(data)
+        assert isinstance(model, ConditionalAdversaryPair)
+
+        G, D = model.generator, model.discriminator
+
+        # X_data: empirical data to be sent to the discriminator. We'll
+        # make an equal amount of generated data and send this to the
+        # discriminator as well.
+        #
+        # X_condition: Conditional data for each empirical sample.
+        #
+        # TODO something wrong here -- where does X_condition come from
+        # on this invocation?
+        (X_data, X_condition), y = data
+        m = data.shape[space.get_batch_axis()]
+
+        # Expected discriminator output: 1 for real data, 0 for
+        # generated samples
+        y1 = T.alloc(1, m, 1)
+        y0 = T.alloc(0, m, 1)
+
+        S, z, other_layers = g.sample_and_noise(X_condition,
+                                                default_input_include_prob=self.generator_default_input_include_prob,
+                                                default_input_scale=self.generator_default_input_scale,
+                                                all_g_layers=(self.infer_layer is not None))
+
+        if self.noise_both != 0.:
+            rng = MRG_RandomStreams(2014 / 6 + 2)
+            S = S + rng.normal(size=S.shape, dtype=S.dtype) * self.noise_both
+            X_data = X_data + rng.normal(size=X_data.shape, dtype=X_data.dtype) * self.noise_both
+
+        fprop_args = [self.discriminator_default_input_include_prob,
+                      self.discriminator_input_include_probs,
+                      self.discriminator_default_input_scale,
+                      self.discriminator_input_scales]
+
+        # Run discriminator on empirical data (1 expected)
+        y_hat1 = D.dropout_fprop(X_data, *fprop_args)
+
+        # Run discriminator on generated data (0 expected)
+        y_hat0 = D.dropout_fprop(S, *fprop_args)
+
+        # Compute discriminator objective
+        d_obj = 0.5 * (D.layers[-1].cost(y1, y_hat1) + d.layers[-1].cost(y0, y_hat0))
+
+        # Compute generator objective
+        if self.no_drop_in_d_for_g:
+            y_hat0_no_drop = D.dropout_fprop(S)
+            g_obj = d.layers[-1].cost(y1, y_hat0_no_drop)
+        else:
+            g_obj = d.layers[-1].cost(y1, y_hat0)
+
+        if self.blend_obj:
+            g_obj = (self.zurich_coeff * g_obj - self.minimax_coeff * d_obj) / (self.zurich_coeff + self.minimax_coeff)
+
+        if model.inferer is not None:
+            # Change this if we ever switch to using dropout in the
+            # construction of S.
+            S_nograd = block_gradient(S)  # Redundant as long as we have custom get_gradients
+            pred = model.inferer.dropout_fprop(S_nograd, *fprop_args)
+
+            if self.infer_layer is None:
+                target = z
+            else:
+                target = other_layers[self.infer_layer]
+
+            i_obj = model.inferer.layers[-1].cost(target, pred)
+        else:
+            i_obj = 0
+
+        return S, d_obj, g_obj, i_obj

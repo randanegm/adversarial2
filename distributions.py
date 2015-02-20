35e141df86c3b593dade12d4e4404cfdea13b489
Jon Gauthier <jon@gauthiers.net>
2015-02-19 21:23:18 -0800

    OneHotDistribution fixes

--- distributions.py
+++ distributions.py
@@ -17,17 +17,17 @@ class Distribution(object):
         raise NotImplementedError("abstract method")
 
 
-class OneHotDistribution(object):
+class OneHotDistribution(Distribution):
     """Randomly samples from a distribution of one-hot vectors."""
 
-    def __init__(self, space, rng=None, dtype='int64'):
+    def __init__(self, space, rng=None):
         super(OneHotDistribution, self).__init__(space)
 
         self.dim = space.get_total_dimension()
-        self.formatter = OneHotFormatter(self.dim, dtype=dtype)
+        self.formatter = OneHotFormatter(self.dim, dtype=space.dtype)
 
         self.rng = RandomStreams() if rng is None else rng
 
     def sample(self, n):
         idxs = self.rng.random_integers((n, 1), low=0, high=self.dim - 1)
-        return theano.shared(np.array([self.formatter.format(idx_arr) for idx_arr in idxs]))
+        return self.formatter.theano_expr(idxs, mode='concatenate')

7a09ee3ebf72bb2644f40b9ded208b266cfb6ff1
Jon Gauthier <jon@gauthiers.net>
2015-02-21 11:42:22 -0800

    Add model visualizer for CIFAR conditional

---
+++ show_samples_cifar_conditional.py
@@ -0,0 +1,54 @@
+from pylearn2.utils import serial
+import sys
+_, model_path = sys.argv
+model = serial.load(model_path)
+space = model.generator.get_output_space()
+from pylearn2.config import yaml_parse
+from pylearn2.format.target_format import OneHotFormatter
+from pylearn2.gui.patch_viewer import PatchViewer
+import numpy as np
+
+dataset = yaml_parse.load(model.dataset_yaml_src)
+
+grid_shape = None
+
+# Number of choices for one-hot values
+rows = model.generator.condition_space.get_total_dimension()
+
+# Samples per condition
+sample_cols = 5
+
+# Generate conditional information
+formatter = OneHotFormatter(rows,
+                            dtype=model.generator.condition_space.dtype)
+conditional = formatter.format(np.concatenate([np.repeat(i, sample_cols) for i in range(rows)]),
+                               mode='concatenate')
+
+# For some reason format_as from VectorSpace is not working right
+topo_samples = model.generator.sample(conditional).eval()
+samples = dataset.get_design_matrix(topo_samples)
+dataset.axes = ['b', 0, 1, 'c']
+dataset.view_converter.axes = ['b', 0, 1, 'c']
+topo_samples = dataset.get_topological_view(samples)
+
+pv = PatchViewer(grid_shape=(rows, sample_cols + 1), patch_shape=(32,32),
+        is_color=True)
+scale = np.abs(samples).max()
+
+X = dataset.X
+topo = dataset.get_topological_view()
+index = 0
+for i in xrange(samples.shape[0]):
+    topo_sample = topo_samples[i, :, :, :]
+    print topo_sample.min(), topo_sample.max()
+    pv.add_patch(topo_sample / scale, rescale=False)
+
+    if (i +1) % sample_cols == 0:
+        sample = samples[i, :]
+        dists = np.square(X - sample).sum(axis=1)
+        j = np.argmin(dists)
+        match = topo[j, :]
+        print match.min(), match.max()
+        pv.add_patch(match / scale, rescale=False, activation=1)
+
+pv.show()

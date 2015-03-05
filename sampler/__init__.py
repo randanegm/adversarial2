726eb60228a86a5ef9cc3f78b7b88c7b76ef0b92
Jon Gauthier <jon@gauthiers.net>
2015-03-04 20:27:18 -0800

    Factor out some sampler functions into adversarial.sampler init module

---
+++ sampler/__init__.py
@@ -0,0 +1,45 @@
+"""
+Defines shared utilities for sampling from a vGAN or cGAN generator
+component.
+"""
+
+import numpy as np
+
+
+def sample_conditional_fix_random(generator, m, n, noise_range=1):
+    """
+    Sample `m * n` points in condition space by sampling `m` points
+    and adding small random noise `n` times for each point.
+    """
+
+    conditional_data = generator.condition_distribution.sample(m).eval()
+    conditional_dim = conditional_data.shape[1]
+    conditional_data = (conditional_data.reshape((m, 1, conditional_dim)).repeat(n, axis=1)
+                                        .reshape((m * n, conditional_dim)))
+    conditional_data += noise_range * (np.random.rand(*conditional_data.shape) * 2. - 1.)
+
+    return conditional_data
+
+
+def sample_conditional_fix_embeddings(generator, m, n,
+                                      embedding_file='/afs/cs.stanford.edu/u/jgauthie/scr/lfw-lsa/LFW_attributes_30d.npz',
+                                      noise_range=1):
+    """
+    Sample `m * n` points in condition space by retrieving `m` points
+    from a provided dataset and adding small random noise `n - 1` times
+    for each point.
+    """
+
+    embs = np.load(embedding_file)['arr_0']
+    np.random.shuffle(embs)
+
+    source_points = embs[:m]
+    dim = source_points.shape[1]
+
+    noisy_points = np.copy(source_points).reshape((m, 1, dim)).repeat(n - 1, axis=1)
+    noisy_points += noise_range * (np.random.rand(*noisy_points.shape) * 2. - 1.)
+
+    ret = (np.concatenate((source_points.reshape((m, 1, dim)), noisy_points), axis=1)
+             .reshape((m * n, dim)))
+
+    return np.cast['float32'](ret)
--- sampler/show_samples_lfw_conditional.py
+++ sampler/show_samples_lfw_conditional.py
@@ -7,51 +7,14 @@ from pylearn2.gui.patch_viewer import PatchViewer
 from pylearn2.utils import serial
 import theano
 
-
-def sample_conditional_fix_random(generator, m, n, noise_range=1):
-    """
-    Sample `m * n` points in condition space by sampling `m` points
-    and adding small random noise `n` times for each point.
-    """
-
-    conditional_data = generator.condition_distribution.sample(m).eval()
-    conditional_dim = conditional_data.shape[1]
-    conditional_data = (conditional_data.reshape((m, 1, conditional_dim)).repeat(n, axis=1)
-                                        .reshape((m * n, conditional_dim)))
-    conditional_data += noise_range * (np.random.rand(*conditional_data.shape) * 2. - 1.)
-
-    return conditional_data
-
-
-def sample_conditional_fix_embeddings(generator, m, n,
-                                      embedding_file='/afs/cs.stanford.edu/u/jgauthie/scr/lfw-lsa/LFW_attributes_30d.npz',
-                                      noise_range=1):
-    """
-    Sample `m * n` points in condition space by retrieving `m` points
-    from a provided dataset and adding small random noise `n - 1` times
-    for each point.
-    """
-
-    embs = np.load(embedding_file)['arr_0']
-    np.random.shuffle(embs)
-
-    source_points = embs[:m]
-    dim = source_points.shape[1]
-
-    noisy_points = np.copy(source_points).reshape((m, 1, dim)).repeat(n - 1, axis=1)
-    noisy_points += noise_range * (np.random.rand(*noisy_points.shape) * 2. - 1.)
-
-    ret = (np.concatenate((source_points.reshape((m, 1, dim)), noisy_points), axis=1)
-             .reshape((m * n, dim)))
-
-    return np.cast['float32'](ret)
+from adversarial import sampler
 
 
 # Build string mapping for conditional samplers so that they can be
 # triggered from CLI
 conditional_samplers = {
-    'fix_random': sample_conditional_fix_random,
-    'fix_embeddings': sample_conditional_fix_embeddings,
+    'fix_random': sampler.sample_conditional_fix_random,
+    'fix_embeddings': sampler.sample_conditional_fix_embeddings,
 }
 
 

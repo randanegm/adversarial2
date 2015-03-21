689d95c404251a0ae95884a3d7e2a86b461c5813
Jon Gauthier <jon@gauthiers.net>
2015-03-21 11:55:48 -0700

    Make this a real generative RNN, indexing into some embedding space

--- rnn.py
+++ rnn.py
@@ -3,52 +3,65 @@ from collections import OrderedDict
 import functools
 
 import numpy as np
-from pylearn2.models.mlp import Layer
+from pylearn2.models.mlp import MLP, Layer, Softmax
+from pylearn2.sandbox.nlp.linear.matrixmul import MatrixMul
+from pylearn2.space import CompositeSpace, VectorSpace
 from pylearn2.utils import sharedX
+from pylearn2.utils.rng import make_theano_rng
 import scipy
 import theano
 import theano.tensor as T
 
 
-class GenerativeRecurrentLayer(Layer):
-    def __init__(self, dim, layer_name, max_steps,
-                 irange=0.1, init_bias=0., nonlinearity=T.tanh,
+class GenerativeRecurrentMLP(MLP):
+    def __init__(self, input_space, hidden_mlp, output_mlp, max_steps, irange,
+                 num_classes, emb_irange=0.1,
                  *args, **kwargs):
-        self.dim = dim
-        self.layer_name = layer_name
         self.max_steps = max_steps
-        self.irange = irange
-        self.init_bias = init_bias
-        self.nonlinearity = nonlinearity
-
         self._scan_updates = OrderedDict()
 
-        super(GenerativeRecurrentLayer, self).__init__(*args, **kwargs)
+        self.irange = irange
+        self.num_classes = num_classes
+        self.emb_dim = input_space.dim
+        self.emb_irange = emb_irange
+
+        self.hidden_mlp = hidden_mlp
+        self.output_mlp = output_mlp
+
+        self.softmax = Softmax(layer_name='softmax',
+                               irange=irange,
+                               binary_target_dim=1,
+                               n_classes=num_classes,)
+
+        layers = [self.hidden_mlp, self.output_mlp, self.softmax]
+        super(GenerativeRecurrentMLP, self).__init__(layers, input_space=input_space,
+                                                     *args, **kwargs)
+
+        self.hidden_dim = hidden_mlp.layers[-1].dim
+        self.set_input_space(input_space)
+        hidden_mlp.set_input_space(CompositeSpace(components=[VectorSpace(dim=self.emb_dim),
+                                                              VectorSpace(dim=self.hidden_dim)]))
 
     @functools.wraps(Layer.set_input_space)
     def set_input_space(self, space):
         self.input_space = space
-        self._params = self._init_params()
+        self.output_space = VectorSpace(dim=self.emb_dim)
 
-    def _init_params(self):
-        input_dim = self.input_space.dim
-        rng = self.mlp.rng
+        # Initialize embeddings
+        E = self.rng.uniform(-self.emb_irange, self.emb_irange, (self.num_classes, self.emb_dim))
+        self.E = sharedX(E, name='embeddings')
 
-        # W: input -> hidden
-        W = rng.uniform(-self.irange, self.irange, (input_dim, self.dim))
+        # Initialize input -> hidden0 mapping
+        W = self.rng.uniform(-self.irange, self.irange, (self.input_space.dim, self.hidden_dim))
 
-        # U: hidden(n) -> hidden(n+1)
-        U = rng.randn(self.dim, self.dim)
-        U, _ = scipy.linalg.qr(U)
-
-        # bias
-        b = np.zeros((self.dim,)) + self.init_bias
-
-        return [
-            sharedX(W, name=(self.layer_name + '_W')),
-            sharedX(U, name=(self.layer_name + '_U')),
-            sharedX(b, name=(self.layer_name + '_b'))
+        self._params = [
+            self.E,
+            sharedX(W, name='inp2hidden0'),
         ]
+
+    @functools.wraps(Layer.get_params)
+    def get_params(self):
+        return self._params + super(GenerativeRecurrentMLP, self).get_params()
 
     @functools.wraps(Layer._modify_updates)
     def _modify_updates(self, updates):
@@ -58,20 +71,27 @@ class GenerativeRecurrentLayer(Layer):
     def fprop(self, state_below):
         self.input_space.validate(state_below)
 
-        W, U, b = self._params
+        _, W = self._params
 
         # First map input -> hidden
-        state_start = T.dot(state_below, W) + b
+        # TODO bias?
+        state_start = T.dot(state_below, W)
 
         # Now scan!
         z, updates = theano.scan(fn=self.fprop_step,
-                                 outputs_info=state_start,
+                                 outputs_info=(state_below, state_start),
                                  n_steps=self.max_steps)
 
         self._scan_updates.update(updates)
         return z
 
-    def fprop_step(self, state_before):
-        # TODO stop condition
-        _, U, _ = self._params
-        return self.nonlinearity(T.dot(state_before, U))
+    def fprop_step(self, state_below, state_before):
+        new_hidden = self.hidden_mlp.fprop((state_below, state_before))
+        output = self.output_mlp.fprop(new_hidden)
+
+        # Fetch new embedding
+        # softmax_out = self.softmax.fprop(output)
+        # print softmax_out, softmax_out.dtype, T.argmax(softmax_out)
+        new_input = self.E[T.argmax(output, axis=0)]
+
+        return new_input, new_hidden
--- tests/test_rnn.py
+++ tests/test_rnn.py
@@ -1,31 +1,47 @@
 
 import numpy as np
-from pylearn2.models.mlp import MLP
+from pylearn2.models.mlp import MLP, Tanh
 from pylearn2.space import VectorSpace
 import theano
 import theano.tensor as T
 
-from adversarial.rnn import GenerativeRecurrentLayer
+from adversarial.rnn import GenerativeRecurrentMLP
 
 
 def test_rnn_generator():
-    n_steps = 10
-    generator = MLP(input_space=VectorSpace(dim=1),
-                    layers=[GenerativeRecurrentLayer(layer_name='recurrent',
-                                                     max_steps=n_steps,
-                                                     dim=1)])
+    # hidden->hidden nested MLP
+    hidden_mlp = MLP(layer_name='hidden->hidden',
+                     layers=[Tanh(layer_name='hidden->hidden_tanh', irange=0., dim=1)])
 
-    generator.layers[0].get_params()[0].set_value(np.ones((1, 1)))
-    generator.layers[0].get_params()[1].set_value(np.ones((1, 1)))
+
+
+    # hidden->output nested MLP
+    output_mlp = MLP(layer_name='hidden->output',
+                     layers=[Tanh(layer_name='hidden->output_tanh', irange=0., dim=1)])
+
+    n_steps = 10
+    generator = GenerativeRecurrentMLP(input_space=VectorSpace(dim=1),
+                                       hidden_mlp=hidden_mlp,
+                                       output_mlp=output_mlp,
+                                       max_steps=n_steps,
+                                       irange=0.01,
+                                       num_classes=100)
+
+    hidden_mlp.layers[0].get_params()[0].set_value(np.ones((2, 1)))
+    output_mlp.layers[0].get_params()[0].set_value(np.ones((1, 1)))
+
+    generator.get_params()[1].set_value(np.ones((1, 1)))
 
     inp = T.matrix()
     f = theano.function([inp], generator.fprop(inp))
 
-    expected = []
-    x = 1
-    for i in range(n_steps):
-        x = np.tanh(x)
-        expected.append(x)
+    outputs, hiddens = f(np.ones((1, 1)))
 
-    np.testing.assert_array_almost_equal(f(np.ones((1, 1))).flatten(),
-                                         expected)
+    expected_hiddens = []
+    x = np.tanh(2)
+    for out in outputs:
+        expected_hiddens.append(x)
+        x = np.tanh(x + out)
+
+    np.testing.assert_array_almost_equal(hiddens.flatten(),
+                                         expected_hiddens)
